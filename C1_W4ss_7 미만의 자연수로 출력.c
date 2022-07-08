7d8c524e84be14c9ae75eba965d09602cb2612ca
harryputer1214 <108115672+harryputer1214@users.noreply.github.com>
2022-07-08 15:40:28 +0900

    Create C1_W4ss_7 미만의 자연수로 출력.c

---
+++ C1_W4ss_7 미만의 자연수로 출력.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+  int in=0;
+  int out=0;
+  scanf("%d",&in);
+  out=in%7;
+  printf("%d",out);
+}
+
+//C3_CD-C1 60.p (3.임의의 자연수를 입력받아 7 미만의 자연수로 출력하는 프로그램을 만들어 봅시다.)
