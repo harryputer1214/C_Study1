a09ac33f207ec5286aeb962c191b58cfe77af4ca
harryputer1214 <108115672+harryputer1214@users.noreply.github.com>
2022-07-15 17:33:04 +0900

    Create W7_Let's Start1.c

---
+++ W7_Let's Start1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+    if (1)
+    {
+        printf("True\t참\n");
+        printf("응\t맞아\n");
+    }
+    else
+    {
+        printf("False\t거짓\n");
+        printf("아니\t아니야\n");
+    }
+}
+
+//W7_Let's Start1 다음 프로그램을 싷행해 봅시다.
