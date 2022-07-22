f63db327d8a3c5507403bea520a054757de3b1ea
harryputer1214 <108115672+harryputer1214@users.noreply.github.com>
2022-07-22 15:15:21 +0900

    Create w6ss_FurtherDiscusson2.c

---
+++ w6ss_FurtherDiscusson2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() 
+{
+  int big=5730;
+  int small=5730;
+  int m=big-small;
+  printf("%d",m);
+
+  //w6ss_FurtherDiscusson2 2.다음 숫자를 서로 다른 변수에 대입한 후 변수를 한 번씩 사용하여 만들 수 있는 네 자리 수 중에서 두 번째로 큰 수와 세 번째로 작은 수의 차를 출력해 봅시다. 
+  //[7] [5] [3] [0]
+}
