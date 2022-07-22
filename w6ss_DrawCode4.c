4b651cb3c7c4eb6cb00a33a182d39df0806e72b4
harryputer1214 <108115672+harryputer1214@users.noreply.github.com>
2022-07-22 14:51:10 +0900

    Create w6ss_DrawCode4.c

---
+++ w6ss_DrawCode4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() 
+{
+  //Problem 1
+  int a=45;
+  a++;
+  a++;
+  a++;
+  a-=5;
+  a-=5;
+  a--;
+  a--;
+  printf("Problem 1: %d\n",a);
+
+  //Problem 2
+  int b=47;
+  b+=5;
+  b++;
+  b+=5;
+  a--;
+  a--;
+  a-=5;
+  printf("Problem 2: %d\n",b);
+
+  //w6ss DrawCode4
+}
