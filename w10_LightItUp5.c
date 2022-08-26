af77c3a3f35631a952bdeaf8492c711dba4e09ea
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-08-26 17:35:36 +0900

    Create w10_LightItUp5.c

---
+++ w10_LightItUp5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+  int i;
+  int j;
+  
+  for (i=5;i>0;i--){
+    for (j=i;j<=5;j++){
+      printf("%d",j);
+    }
+    printf("\n");
+  }
+}
+
+//w10_LightItUp5
