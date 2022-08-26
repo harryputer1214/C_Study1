16fb555a3662a7881c1ec8250055e372c1b39075
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-08-26 16:44:01 +0900

    Create w11_LightItUp1.c

---
+++ w11_LightItUp1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() {
+  int i;
+  int j;
+  for (i=1;i<7;i++){
+    printf("[ %d학년 ] ",i);
+    for (j=1;j<8;j++){
+      printf("[ %d반 ] ",j);
+    }
+    printf("\n");
+  }
+}
+
+//w11_LightItUp1
