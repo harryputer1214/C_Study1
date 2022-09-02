ef3fdd73d5f5927a31d41a1a33752949b78d1873
HaeChan Jeong <108115672+harryputer1214@users.noreply.github.com>
2022-09-02 16:39:24 +0900

    Create w11_LightItUp1😊🤣🤣.c

---
+++ w11_LightItUp1😊🤣🤣.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void plus(int num1, int num2){
+  printf("%d\n",num1 + num2);
+}
+
+void main() {
+  int a = 3;
+  int b = 2;
+  plus(a, b);
+  plus(5, 3);
+}
+
+//w11_LightItUp1
